a0fb75cf9d0660be44a1ae3e00fb75a370171181
ervin stewart <epluribusfungo@gmail.com>
2020-03-10 14:13:56 -0400

    implemented Swagger on employee service

--- src/main/java/com/revature/rms/employee/EmployeeServiceApplication.java
+++ src/main/java/com/revature/rms/employee/EmployeeServiceApplication.java
@@ -3,8 +3,15 @@ package com.revature.rms.employee;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableEurekaClient
+@EnableSwagger2
 @SpringBootApplication
 public class EmployeeServiceApplication {
 // test
@@ -12,4 +19,13 @@ public class EmployeeServiceApplication {
 		SpringApplication.run(EmployeeServiceApplication.class, args);
 	}
 
+	@Bean
+	public Docket swagger(){
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.basePackage(this.getClass().getPackage().getName()))
+				.paths(PathSelectors.any())
+				.build()
+				.useDefaultResponseMessages(false);
+	}
 }
